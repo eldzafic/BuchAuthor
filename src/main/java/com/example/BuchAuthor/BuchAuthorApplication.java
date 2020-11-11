850ad67dd480ed7a29b4de9188bf270518b350bb
Elmedin Dzafic <eldzafic@tsn.at>
2020-11-11 14:37:13 +0100

    Initial commit

---
+++ src/main/java/com/example/BuchAuthor/BuchAuthorApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuchAuthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchAuthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchAuthorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuchAuthor/BuchAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuchAuthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuchAuthorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
